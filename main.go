a9b4ecf3ed6bb32caeef71678b5d7aa6d3b87c02
Reiven Candra Hamid <51397165+rvreich@users.noreply.github.com>
2019-09-03 12:36:40 +0700

    Update main.go

--- main.go
+++ main.go
@@ -18,11 +18,11 @@ func startSession(){
 	client , _ = mongo.Connect(context.Background(),clientOptions)
 
 	r := mux.NewRouter()
-	w := WorldExperience{}
+	//w := WorldExperience{}
 	pr := PlaceRecommendation{}
 	r.HandleFunc("/world-experiences",w.GetExperiences).Methods("GET")
-	r.HandleFunc("/world-place-recommendations",pr.GetWorldRecommendation).Methods("GET")
-	r.HandleFunc("/bandung-place-recommendations",pr.GetBandungRecommendation).Methods("GET")
+	//r.HandleFunc("/world-place-recommendations",pr.GetWorldRecommendation).Methods("GET")
+	//r.HandleFunc("/bandung-place-recommendations",pr.GetBandungRecommendation).Methods("GET")
 	fmt.Println("Starting Session")
 	http.ListenAndServe(":3001",r)
 }
