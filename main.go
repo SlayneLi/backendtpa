c205394c6c89263f19c7d957a35c16ce1f0b1fe6
Reiven Candra Hamid <51397165+rvreich@users.noreply.github.com>
2019-09-03 13:11:06 +0700

    updated main.go

--- main.go
+++ main.go
@@ -20,9 +20,9 @@ func startSession(){
 	w := WorldExperience{}
 	pr := PlaceRecommendation{}
 	r.HandleFunc("/world-experiences",w.GetExperiences).Methods("GET")
+	r.HandleFunc("/bandung-place-recommendation/{id}",pr.GetBandungRecommendation).Methods("GET")
 	r.HandleFunc("/world-place-recommendations",pr.GetWorldRecommendation).Methods("GET")
 	r.HandleFunc("/bandung-place-recommendations",pr.GetBandungRecommendations).Methods("GET")
-	r.HandleFunc("/bandung-place-recommendations/{id}",pr.GetBandungRecommendation).Methods("GET")
 	fmt.Println("Starting Session")
 	http.ListenAndServe(":3001",r)
 }
