f49d6419cce74d01f5a78b39cd00e86741a41388
Reiven Candra Hamid <51397165+rvreich@users.noreply.github.com>
2019-09-03 13:07:44 +0700

    updated mongo files
    updated necessary files

--- main.go
+++ main.go
@@ -21,7 +21,8 @@ func startSession(){
 	pr := PlaceRecommendation{}
 	r.HandleFunc("/world-experiences",w.GetExperiences).Methods("GET")
 	r.HandleFunc("/world-place-recommendations",pr.GetWorldRecommendation).Methods("GET")
-	r.HandleFunc("/bandung-place-recommendations",pr.GetBandungRecommendation).Methods("GET")
+	r.HandleFunc("/bandung-place-recommendations",pr.GetBandungRecommendations).Methods("GET")
+	r.HandleFunc("/bandung-place-recommendations/{id}",pr.GetBandungRecommendation).Methods("GET")
 	fmt.Println("Starting Session")
 	http.ListenAndServe(":3001",r)
 }
