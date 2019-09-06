be498c5110061c9034cc0dfb0c04ee624ab76058
Reiven Candra Hamid <51397165+rvreich@users.noreply.github.com>
2019-09-06 16:36:27 +0700

    updated main.go
    updated routing for hosting purpose

--- main.go
+++ main.go
@@ -24,7 +24,6 @@ func startSession() {
 	//r.HandleFunc("/world-place-recommendations", pr.GetWorldRecommendation).Methods("GET")
 	r.HandleFunc("/get-places", p.getPlaces).Methods("GET")
 	r.HandleFunc("/insert-place", p.insertPlace).Methods("POST")
-	r.HandleFunc("/insert-place-review/{id}", p.insertReview).Methods("POST")
 
 	r.HandleFunc("/get-experiences", e.getExperiences).Methods("GET")
 	r.HandleFunc("/insert-experience", e.insertExperience).Methods("POST")
