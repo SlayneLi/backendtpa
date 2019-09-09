c6341cee5383b681900d998efeac25fffe138ed2
Reiven Candra Hamid <51397165+rvreich@users.noreply.github.com>
2019-09-09 17:29:04 +0700

    fixing public hosting for mySQL

--- database.go
+++ database.go
@@ -7,17 +7,24 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+const DBUSERNAME = "root"
+const DBPASSWORD = ""
+const DBSERVER = "kentang.online"
+const DBSERVERPORT = "3306"
+const DBNAME = "airbnb"
+
 type DbHandler struct {
 	ConnectionString string
 }
 
 func (db *DbHandler) connect() *sql.DB {
-	con, err := sql.Open("mysql", "root:root@tcp(kentang.online:3306)/airbnb")
+	db.ConnectionString = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",DBUSERNAME,DBPASSWORD,DBSERVER,DBSERVERPORT,DBNAME)
+	con, err := sql.Open("mysql", db.ConnectionString)
 	if err != nil {
 		log.Panic(err.Error())
 	}
 	err = con.Ping()
-	if(err != nil){
+	if err != nil {
 		fmt.Println("MySQL is not connected")
 	}
 	return con
