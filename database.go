6f245e1ae2a5a838fc25fbdc05e0d4392d67481d
Reiven Candra Hamid <51397165+rvreich@users.noreply.github.com>
2019-09-09 10:31:18 +0700

    Updated content of some files
    updated database.go content for public hosting purpose

--- database.go
+++ database.go
@@ -7,19 +7,12 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-const DBUSERNAME = "root"
-const DBPASSWORD = ""
-const DBSERVER = "127.0.0.1"
-const DBSERVERPORT = "3306"
-const DBNAME = "airbnb"
-
 type DbHandler struct {
 	ConnectionString string
 }
 
 func (db *DbHandler) connect() *sql.DB {
-	//db.ConnectionString = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", DBUSERNAME, DBPASSWORD, DBSERVER, DBSERVERPORT, DBNAME,)
-	con, err := sql.Open("mysql", "root:@tcp(localhost:3306)/airbnb")
+	con, err := sql.Open("mysql", "root:root@tcp(kentang.online:3306)/airbnb")
 	if err != nil {
 		log.Panic(err.Error())
 	}
